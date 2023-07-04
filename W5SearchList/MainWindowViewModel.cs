e4f2a0547034b632709c8f0417bc9b6426197fcd
Owais.Sayed <Owais.Sayed@PC-33>
2023-07-04 12:18:23 +0530

    Add project files.

---
+++ W5SearchList/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace W5SearchList
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public SearchableViewModel SearchList1 { get; set; }
+        public SearchableViewModel SearchList2 { get; set; }
+        public SearchableViewModel SearchList3 { get; set; }
+        public List<Product> ProductList { get; set; }
+        public List<Order> OrderList { get; set; }
+        public MainWindowViewModel()
+        {
+            ProductList = new List<Product>() {
+                new Product() { ProductID = 1, ProductName = "Milkshake"},
+                new Product() { ProductID = 2, ProductName = "Apple Juice"},
+                new Product() { ProductID = 3, ProductName = "Mocktail"},
+                new Product() { ProductID = 4, ProductName = "Cocktail"},
+                new Product() { ProductID = 5, ProductName = "On the Rocks"},
+            };
+            OrderList = new List<Order>()
+            {
+                new Order() { OrderID = 1001, Location = "Mumbai"},
+                new Order() { OrderID = 1002, Location = "Alibaug"},
+                new Order() { OrderID = 1003, Location = "Pune"},
+                new Order() { OrderID = 1004, Location = "Konkan"},
+                new Order() { OrderID = 1005, Location = "Lonavla"},
+            };
+            List<string> friends = new List<string>()
+            {
+                "Owais", "Amey", "Ajay", "Sagar"
+            };
+            SearchList1 = new SearchableViewModel<Product>(ProductList);
+            SearchList2 = new SearchableViewModel<string>(friends);
+            SearchList3 = new SearchableViewModel<Order>(OrderList);
+        }        
+    }
+}
---
+++ W5SearchList/SearchableViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5SearchList
+{
+    public class SearchableViewModel<T>: BaseViewModel
+    {
+        public SearchableViewModel(List<T> list)
+        {
+            MainList = new ObservableCollection<T>();
+            foreach (var item in list)
+            {
+                MainList.Add(item);
+            }
+            InitialisingDisplayList(null);
+        }
+        private string _searchValue;
+        public string SearchValue
+        {
+            get
+            {
+                return _searchValue;
+            }
+            set
+            {
+                InitialisingDisplayList(value);
+                _searchValue = value;
+            }
+        }
+        public void InitialisingDisplayList(string search)
+        {
+            DisplayList = new ObservableCollection<string>();
+            if (string.IsNullOrEmpty(search))
+            {
+                foreach (var item in MainList)
+                {
+                    foreach (var item1 in item.GetType().GetProperties())
+                    {
+                        if ((item1.PropertyType == typeof(string)))
+                        {
+
+                        }
+                    }
+                    
+                    DisplayList.Add(item);
+                }
+            }
+            else
+            {
+                foreach (var item in MainList)
+                {
+                    if (item.ToLower().Contains(search.ToLower()))
+                    {
+                        DisplayList.Add(item);
+                    }
+                }
+            }
+            OnPropertyChanged(nameof(DisplayList));
+        }
+        public ObservableCollection<string> DisplayList { get; set; }
+        public ObservableCollection<T> MainList { get; set; }
+    }
+}
